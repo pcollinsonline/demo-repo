f0bd04f70ab26ea3c2c1d32441cf1fdf90e04bad
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-12-28 09:18:11 -0500

    feat(infrastructure/cdk-app): integrate `AppStagingSynthesizer` and add `cdk-nag`
    * Add `AppStagingSynthesizer` for improved resource management and staging configuration.
    * Introduce `cdk-nag` for security and compliance validation.
    * Update scripts and dependencies, including TypeScript and package adjustments.
    * Mark `src/index.ts` as executable.

--- infrastructure/cdk-app/src/index.ts
+++ infrastructure/cdk-app/src/index.ts
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+import { AppStagingSynthesizer } from '@aws-cdk/app-staging-synthesizer-alpha'
+import { BucketEncryption } from 'aws-cdk-lib/aws-s3'
 import os from 'node:os'
 
 import { BaseApp } from '@packages/aws-cdk-lib'
@@ -14,6 +17,11 @@ const app = new BaseApp({
     region: 'us-east-1',
     stage: CDK_STAGE,
   },
+  defaultStackSynthesizer: AppStagingSynthesizer.defaultResources({
+    appId: 'CachedDockerImageDemo',
+    imageAssetVersionCount: 10, // Keep 10 latest images
+    stagingBucketEncryption: BucketEncryption.S3_MANAGED,
+  }),
 })
 
 new AppStack(app, 'AppStack', {
