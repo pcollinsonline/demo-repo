30c17edf2b0a7713093137b83f35f8c85472d33c
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-12-31 18:05:42 -0500

    feat(infrastructure/cdk-app): add custom logical `ID` allocation support in `BaseStack`
    * Introduce `allocateLogicalId` method to prepend a configurable prefix to logical IDs.
    * Include documentation for setting the prefix using the `cdk --context` flag.

--- packages/aws-cdk-lib/src/base-stack.ts
+++ packages/aws-cdk-lib/src/base-stack.ts
@@ -1,9 +1,18 @@
 import type { Construct } from 'constructs'
 
-import { type StackProps, Stack } from 'aws-cdk-lib'
+import { type CfnElement, type StackProps, Stack } from 'aws-cdk-lib'
 
 import { nameIt } from './utils.js'
 
+/**
+ * A base stack class that implements custom logical name
+ * allocation. Adds a prefix if it is defined in the "prefix"
+ * context key.
+ *
+ * see https://github.com/aws-samples/aws-cdk-examples/blob/main/typescript/custom-logical-names/base-stack.ts
+ *
+ * Use `cdk --context prefix=PREFIX` to set the prefix.
+ */
 export class BaseStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, nameIt(scope, id), {
@@ -14,5 +23,11 @@ export class BaseStack extends Stack {
       },
       terminationProtection: false,
     })
+  }
+
+  public override allocateLogicalId(element: CfnElement): string {
+    const orig = super.allocateLogicalId(element)
+    const prefix = this.node.tryGetContext('prefix') as string | undefined
+    return prefix ? prefix + orig : orig
   }
 }
